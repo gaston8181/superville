27844363028f7b30ef3478efbc518e7694e8df7f
gaston <agui.gaston@gmail.com>
2020-02-05 13:02:39 -0300

    proyecto inicial

---
+++ src/main/java/com/gaston/superville/SupervilleApplication.java
@@ -0,0 +1,13 @@
+package com.gaston.superville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupervilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaston/superville/SupervilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaston.superville;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
